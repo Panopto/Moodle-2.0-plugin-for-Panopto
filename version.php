999d00b9a08f70c307811573e4735c8d098d75f5
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2015-03-09 11:45:45 +0000

    Depricate cron function.
    Since events tracking has been added (aka rollingsync), there is no need to
    run cron to sync enrolments. Besides, cron function is broken anyway for a
    while (after introduction of multiple panopto servers
    $panopto_data->servername is undefined unless courseid has been specified).

--- version.php
+++ version.php
@@ -28,9 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015030300;
+$plugin->version = 2015030301;
 $plugin->requires = 2010112400;  // Version 2.0
-$plugin->cron = 0;
 $plugin->component = 'block_panopto';
 $plugin->maturity = MATURITY_STABLE;
 
