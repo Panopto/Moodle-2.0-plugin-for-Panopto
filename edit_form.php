c23ad361a200e45ba3cec87a737199e41ba46429
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2015-03-16 14:07:50 +0000

    Fix provisioning link within the provisioned block.
    To be consistent with new block provisioning, the link to re-provision the
    block should use provision_course_internal.php. Also refactored the code to
    use html_writer and moodle_url methods.

--- edit_form.php
+++ edit_form.php
@@ -45,15 +45,12 @@ class block_panopto_edit_form extends block_edit_form {
         if ($panopto_data->is_provisioned()) {
             $mform->addElement('header', 'configheader', get_string('block_edit_header', 'block_panopto'));
 
-            $params = new stdClass;
-            $params->course_id = $COURSE->id;
-            $params->return_url = $_SERVER['REQUEST_URI'];
-            $query_string = http_build_query($params, '', '&');
-            $provision_url = "$CFG->wwwroot/blocks/panopto/provision_course.php?" . $query_string;
+            $provision_url = html_writer::link(new moodle_url('/blocks/panopto/provision_course_internal.php',
+                    array('id' => $COURSE->id, 'return_url' => $_SERVER['REQUEST_URI'])),
+                    get_string('add_to_panopto', 'block_panopto'));
 
-            $add_to_panopto = get_string('add_to_panopto', 'block_panopto');
             $or = get_string('or', 'block_panopto');
-            $mform->addElement('html', "<a href='$provision_url'>$add_to_panopto</a><br><br>-- $or --<br><br>");
+            $mform->addElement('html', $provision_url . "<br><br>-- $or --<br><br>");
 
             $course_list = $panopto_data->get_course_options();
             $mform->addElement('selectgroups', 'config_course', get_string('existing_course', 'block_panopto'), $course_list['courses']);
