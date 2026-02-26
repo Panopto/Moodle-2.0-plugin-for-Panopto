54a6e968d2bd78ca8af8ad34e393537829f269e4
Jonathan Champ <royanee@gmail.com>
2026-02-26 18:24:43 +0100

    js: skip remove() because html() will automatically clean up

--- amd/src/asyncload.js
+++ amd/src/asyncload.js
@@ -38,11 +38,9 @@ export const initblock = (params) => {
             };
 
             fetchMany([request])[0].then((response) => {
-                mynode.find('.loading_text').remove();
                 mynode.html(response);
                 return response;
             }).catch((error) => {
-                mynode.find('.loading_text').remove();
                 mynode.html(error);
             });
         });
